747764e187ee6daecddb4b0b2d2885b171c6003d
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-04-16 10:54:51 -0300

    Visitor can choose interested tours

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,24 @@
 class HomeController < ApplicationController
 
   def index
+    @interest_tours = Tour.all
   end
 
   def filter_period
-
+    @interest_tours = Tour.all
     if params[:filter_from].present? || params[:filter_to].present?
       @start_date = Date.parse(params[:filter_from])
       @end_date = Date.parse(params[:filter_to])
     else
       @start_date = Date.today
       @end_date = @start_date
-      
+
     end
     @date_range = @start_date.step(@end_date, 1)
     prices_in_period = Price.where("start_date <= ? AND end_date >= ?", @start_date, @end_date)
-    tours_id = prices_in_period.pluck(:tour_id)
+    tours_id = prices_in_period.pluck(:tour_id).uniq
+    tours_id = tours_id & params[:tour_ids].map(&:to_i) if params[:tour_ids].present?
     @tours = Tour.where(id: tours_id)
     render 'index'
   end
-
 end
--- spec/features/visitor_sees_tour_prices_within_period_spec.rb
+++ spec/features/visitor_sees_tour_prices_within_period_spec.rb
@@ -1,153 +1,242 @@
 require 'rails_helper'
 
-  feature 'visitor sees prices witih period' do
+feature 'visitor sees prices witih period' do
 
-    scenario 'successfully' do
+  scenario 'successfully' do
 
-        category = create(:category)
-        tour1 = create(:tour, category: category, name: 'Flutuacao Rio Sucuri')
-        tour2 = create(:tour, category: category, name: 'Cachoeira Boca da Onca')
-        tour3 = create(:tour, category: category, name: 'Rapel Abismo Anhumas')
+    category = create(:category)
+    tour1 = create(:tour, category: category, name: 'Flutuacao Rio Sucuri')
+    tour2 = create(:tour, category: category, name: 'Cachoeira Boca da Onca')
+    tour3 = create(:tour, category: category, name: 'Rapel Abismo Anhumas')
 
-        high_season_price_tour1 = create(:price, tour: tour1,
-                                            start_date: '2017-02-01',
-                                            end_date: '2017-03-01',
-                                            adult_price: 120,
-                                            child_price: 99,
-                                            baby_price: 13,
-                                            season_type: 1)
-        low_season_price_tour1 = create(:price, tour: tour1,
-                                            start_date: '2017-02-01',
-                                            end_date: '2017-03-01',
-                                            adult_price: 90,
-                                            child_price: 59,
-                                            baby_price: 0,
-                                            season_type: 0)
+    high_season_price_tour1 = create(:price, tour: tour1,
+                                        start_date: '2017-02-01',
+                                        end_date: '2017-03-01',
+                                        adult_price: 120,
+                                        child_price: 99,
+                                        baby_price: 13,
+                                        season_type: 1)
+    low_season_price_tour1 = create(:price, tour: tour1,
+                                        start_date: '2017-02-01',
+                                        end_date: '2017-03-01',
+                                        adult_price: 90,
+                                        child_price: 59,
+                                        baby_price: 0,
+                                        season_type: 0)
 
-        high_season_price_tour2 = create(:price, tour: tour2,
-                                            start_date: '2017-02-01',
-                                            end_date: '2017-03-01',
-                                            adult_price: 110,
-                                            child_price: 79,
-                                            baby_price: 25,
-                                            season_type: 1)
-        low_season_price_tour2 = create(:price, tour: tour2,
-                                            start_date: '2017-02-01',
-                                            end_date: '2017-03-01',
-                                            adult_price: 60,
-                                            child_price: 33,
-                                            baby_price: 11,
-                                            season_type: 0)
+    high_season_price_tour2 = create(:price, tour: tour2,
+                                        start_date: '2017-02-01',
+                                        end_date: '2017-03-01',
+                                        adult_price: 110,
+                                        child_price: 79,
+                                        baby_price: 25,
+                                        season_type: 1)
+    low_season_price_tour2 = create(:price, tour: tour2,
+                                        start_date: '2017-02-01',
+                                        end_date: '2017-03-01',
+                                        adult_price: 60,
+                                        child_price: 33,
+                                        baby_price: 11,
+                                        season_type: 0)
 
-        high_season_price_tour3 = create(:price, tour: tour3,
-                                            start_date: '2017-02-01',
-                                            end_date: '2017-03-01',
-                                            adult_price: 898,
-                                            child_price: 630,
-                                            baby_price: 420,
-                                            season_type: 1)
-        low_season_price_tour3 = create(:price, tour: tour3,
-                                            start_date: '2017-02-01',
-                                            end_date: '2017-03-01',
-                                            adult_price: 700,
-                                            child_price: 412,
-                                            baby_price: 134,
-                                            season_type: 0)
+    high_season_price_tour3 = create(:price, tour: tour3,
+                                        start_date: '2017-02-01',
+                                        end_date: '2017-03-01',
+                                        adult_price: 898,
+                                        child_price: 630,
+                                        baby_price: 420,
+                                        season_type: 1)
+    low_season_price_tour3 = create(:price, tour: tour3,
+                                        start_date: '2017-02-01',
+                                        end_date: '2017-03-01',
+                                        adult_price: 700,
+                                        child_price: 412,
+                                        baby_price: 134,
+                                        season_type: 0)
 
-        high_season = create(:high_season, name: 'Carnaval', start_date: '2017-02-10', end_date: '2017-02-18')
+    high_season = create(:high_season, name: 'Carnaval', start_date: '2017-02-10', end_date: '2017-02-18')
 
-        visit root_path
+    visit root_path
 
-        within('div#prices_within_period') do
+    within('div#prices_within_period') do
 
-          fill_in 'Desde:', with: '2017-02-09'
-          fill_in 'Até:',   with: '2017-02-10'
+      fill_in 'Desde:', with: '2017-02-09'
+      fill_in 'Até:',   with: '2017-02-10'
 
-          click_on 'Ver Preços'
-
-        end
-
-        expect(page).to have_css('h2', text: 'Preços para o período entre 09/02/2017 e 10/02/2017')
-
-        within('table#prices_within_period') do
-
-          expect(page).to have_content('Nome do Passeio')
-          expect(page).to have_content('Flutuacao Rio Sucuri')
-          expect(page).to have_content('Cachoeira Boca da Onca')
-          expect(page).to have_content('Rapel Abismo Anhumas')
-
-          expect(page).to have_content('09/02/2017')
-          expect(page).to have_content('Baixa')
-          expect(page).to have_content('R$ 90,00')
-          expect(page).to have_content('R$ 60,00')
-          expect(page).to have_content('R$ 700,00')
-
-          expect(page).to have_content('10/02/2017')
-          expect(page).to have_content('ALTA')
-          expect(page).to have_content('R$ 120,00')
-          expect(page).to have_content('R$ 110,00')
-          expect(page).to have_content('R$ 898,00')
-
-        end
-
-    end #scenario anterior
-
-    scenario 'Without date' do
-
-        category = create(:category)
-        tour1 = create(:tour, category: category, name: 'Flutuacao Rio Sucuri')
-        tour2 = create(:tour, category: category, name: 'Cachoeira Boca da Onca')
-
-        high_season_price_tour1 = create(:price, tour: tour1,
-                                            start_date: Date.today,
-                                            end_date: Date.today,
-                                            adult_price: 120,
-                                            child_price: 99,
-                                            baby_price: 13,
-                                            season_type: 1)
-
-        high_season_price_tour2 = create(:price, tour: tour2,
-                                            start_date: Date.today + 2.weeks,
-                                            end_date: Date.today + 1.month,
-                                            adult_price: 110,
-                                            child_price: 79,
-                                            baby_price: 25,
-                                            season_type: 1)
-
-
-
-        high_season = create(:high_season, name: 'Carnaval', start_date: Date.today, end_date: Date.today + 1.month)
-
-        visit root_path
-
-        within('div#prices_within_period') do
-
-          fill_in 'Desde:', with: ''
-          fill_in 'Até:',   with: ''
-
-          click_on 'Ver Preços'
-
-        end
-
-        expect(page).to have_css('h2', text: "Preços para o período entre #{I18n.l(Date.today)} e #{I18n.l(Date.today)}")
-
-        within('table#prices_within_period') do
-
-          expect(page).to have_content('Nome do Passeio')
-          expect(page).to have_content('Flutuacao Rio Sucuri')
-
-          expect(page).to have_content( I18n.l(Date.today))
-          expect(page).to have_content('ALTA')
-          expect(page).to have_content('R$ 120,00')
-          expect(page).to have_content('R$ 99,00')
-          expect(page).to have_content('R$ 13,00')
-
-          expect(page).not_to have_content('Cachoeira Boca da Onca')
-          expect(page).not_to have_content('R$ 110,00')
-          expect(page).not_to have_content('R$ 79,00')
-          expect(page).not_to have_content('R$ 25,00')
-        end
+      click_on 'Ver Preços'
 
     end
 
+    expect(page).to have_css('h2', text: 'Preços para o período entre 09/02/2017 e 10/02/2017')
+
+    within('table#prices_within_period') do
+
+      expect(page).to have_content('Nome do Passeio')
+      expect(page).to have_content('Flutuacao Rio Sucuri')
+      expect(page).to have_content('Cachoeira Boca da Onca')
+      expect(page).to have_content('Rapel Abismo Anhumas')
+
+      expect(page).to have_content('09/02/2017')
+      expect(page).to have_content('Baixa')
+      expect(page).to have_content('R$ 90,00')
+      expect(page).to have_content('R$ 60,00')
+      expect(page).to have_content('R$ 700,00')
+
+      expect(page).to have_content('10/02/2017')
+      expect(page).to have_content('ALTA')
+      expect(page).to have_content('R$ 120,00')
+      expect(page).to have_content('R$ 110,00')
+      expect(page).to have_content('R$ 898,00')
+
+    end
+
+  end #scenario anterior
+
+  scenario 'Without date' do
+
+    category = create(:category)
+    tour1 = create(:tour, category: category, name: 'Flutuacao Rio Sucuri')
+    tour2 = create(:tour, category: category, name: 'Cachoeira Boca da Onca')
+
+    high_season_price_tour1 = create(:price, tour: tour1,
+                                        start_date: Date.today,
+                                        end_date: Date.today,
+                                        adult_price: 120,
+                                        child_price: 99,
+                                        baby_price: 13,
+                                        season_type: 1)
+
+    high_season_price_tour2 = create(:price, tour: tour2,
+                                        start_date: Date.today + 2.weeks,
+                                        end_date: Date.today + 1.month,
+                                        adult_price: 110,
+                                        child_price: 79,
+                                        baby_price: 25,
+                                        season_type: 1)
+
+
+
+    high_season = create(:high_season, name: 'Carnaval', start_date: Date.today, end_date: Date.today + 1.month)
+
+    visit root_path
+
+    within('div#prices_within_period') do
+
+      fill_in 'Desde:', with: ''
+      fill_in 'Até:',   with: ''
+
+      click_on 'Ver Preços'
+
+    end
+
+    expect(page).to have_css('h2', text: "Preços para o período entre #{I18n.l(Date.today)} e #{I18n.l(Date.today)}")
+
+    within('table#prices_within_period') do
+
+      expect(page).to have_content('Nome do Passeio')
+      expect(page).to have_content('Flutuacao Rio Sucuri')
+
+      expect(page).to have_content( I18n.l(Date.today))
+      expect(page).to have_content('ALTA')
+      expect(page).to have_content('R$ 120,00')
+      expect(page).to have_content('R$ 99,00')
+      expect(page).to have_content('R$ 13,00')
+
+      expect(page).not_to have_content('Cachoeira Boca da Onca')
+      expect(page).not_to have_content('R$ 110,00')
+      expect(page).not_to have_content('R$ 79,00')
+      expect(page).not_to have_content('R$ 25,00')
+    end
+
   end
+
+scenario 'and with only tour of interest' do
+
+  category = create(:category)
+  tour1 = create(:tour, category: category, name: 'Flutuacao Rio Sucuri')
+  tour2 = create(:tour, category: category, name: 'Cachoeira Boca da Onca')
+  tour3 = create(:tour, category: category, name: 'Rapel Abismo Anhumas')
+
+  high_season_price_tour1 = create(:price, tour: tour1,
+                                      start_date: '2017-02-01',
+                                      end_date: '2017-03-01',
+                                      adult_price: 120,
+                                      child_price: 99,
+                                      baby_price: 13,
+                                      season_type: 1)
+  low_season_price_tour1 = create(:price, tour: tour1,
+                                      start_date: '2017-02-01',
+                                      end_date: '2017-03-01',
+                                      adult_price: 90,
+                                      child_price: 59,
+                                      baby_price: 0,
+                                      season_type: 0)
+
+  high_season_price_tour2 = create(:price, tour: tour2,
+                                      start_date: '2017-02-01',
+                                      end_date: '2017-03-01',
+                                      adult_price: 110,
+                                      child_price: 79,
+                                      baby_price: 25,
+                                      season_type: 1)
+  low_season_price_tour2 = create(:price, tour: tour2,
+                                      start_date: '2017-02-01',
+                                      end_date: '2017-03-01',
+                                      adult_price: 60,
+                                      child_price: 33,
+                                      baby_price: 11,
+                                      season_type: 0)
+
+  high_season_price_tour3 = create(:price, tour: tour3,
+                                      start_date: '2017-02-01',
+                                      end_date: '2017-03-01',
+                                      adult_price: 898,
+                                      child_price: 630,
+                                      baby_price: 420,
+                                      season_type: 1)
+  low_season_price_tour3 = create(:price, tour: tour3,
+                                      start_date: '2017-02-01',
+                                      end_date: '2017-03-01',
+                                      adult_price: 700,
+                                      child_price: 412,
+                                      baby_price: 134,
+                                      season_type: 0)
+
+  high_season = create(:high_season, name: 'Carnaval', start_date: '2017-02-10', end_date: '2017-02-18')
+
+  visit root_path
+
+  within('div#prices_within_period') do
+
+    fill_in 'Desde:', with: '2017-02-09'
+    fill_in 'Até:',   with: '2017-02-10'
+    check tour1.name
+    check tour2.name
+
+    click_on 'Ver Preços'
+
+  end
+
+  expect(page).to have_css('h2', text: 'Preços para o período entre 09/02/2017 e 10/02/2017')
+
+  within('table#prices_within_period') do
+
+    expect(page).to have_content('Nome do Passeio')
+    expect(page).to have_content('Flutuacao Rio Sucuri')
+    expect(page).to have_content('Cachoeira Boca da Onca')
+    expect(page).not_to have_content('Rapel Abismo Anhumas')
+
+    expect(page).to have_content('09/02/2017')
+    expect(page).to have_content('Baixa')
+    expect(page).to have_content('R$ 90,00')
+    expect(page).to have_content('R$ 60,00')
+    expect(page).not_to have_content('R$ 700,00')
+
+    expect(page).to have_content('10/02/2017')
+    expect(page).to have_content('ALTA')
+    expect(page).to have_content('R$ 120,00')
+    expect(page).to have_content('R$ 110,00')
+    expect(page).not_to have_content('R$ 898,00')
+    end
+  end
+end
