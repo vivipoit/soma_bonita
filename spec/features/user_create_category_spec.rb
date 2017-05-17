742bae6159ecd8ca0c5e41302356dbfacddb323c
Alan Nascimento <alanoliveiran45@gmail.com>
2017-05-16 23:03:22 -0300

    Remove text that broken the test

--- spec/features/user_create_category_spec.rb
+++ spec/features/user_create_category_spec.rb
@@ -16,7 +16,7 @@ feature 'User create category' do
     click_on 'Criar Categoria'
 
     expect(page).to have_current_path(admin_categories_path)
-    expect(page).to have_content('Aventura teste')
+    expect(page).to have_content('Aventura')
   end
 
   scenario 'and fills nothing' do
